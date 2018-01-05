516b4cbd17b4130cb8001af0e7f1af2889743b12
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2018-01-05 00:13:40 +0000

    tests created for ufc news api

--- lib/services/news_service.rb
+++ lib/services/news_service.rb
@@ -10,10 +10,64 @@ class NewsService
     @news_data = JSON.parse(self.class.get("http://ufc-data-api.ufc.com/api/v3/iphone/news").body)
   end
 
-  # base_uri 'http://ufc-data-api.ufc.com/api/v3/iphone/news'
-
   def get_id
     @news_data[0]['id']
+  end
+
+  def get_external_url_text
+    @news_data[0]['external_url_text']
+  end
+
+  def get_author
+    @news_data[0]['author']
+  end
+
+  def get_title
+    @news_data[0]['title']
+  end
+
+  def get_article_media_id
+    @news_data[0]['article_media_id']
+  end
+
+  def get_article_date
+    @news_data[0]['article_date']
+  end
+
+  def get_thumbnail
+    @news_data[0]['thumbnail']
+  end
+
+  def get_external_url
+    @news_data[0]['external_url']
+  end
+
+  def get_introduction
+    @news_data[0]['introduction']
+  end
+
+  def get_article_fighter_id
+    @news_data[0]['article_fighter_id']
+  end
+
+  def get_featured_news_category
+    @news_data[0]['featured_news_category']
+  end
+
+  def get_last_modified
+    @news_data[0]['last_modified']
+  end
+
+  def get_url_name
+    @news_data[0]['url_name']
+  end
+
+  def get_created
+    @news_data[0]['created']
+  end
+
+  def get_published_start_date
+    @news_data[0]['published_start_date']
   end
 
 end
--- spec/news_spec.rb
+++ spec/news_spec.rb
@@ -10,4 +10,82 @@ describe UfcApi do
     expect(@news.get_id).to be_a(Integer)
   end
 
+  it "should return a string for external_url_text" do
+    if @news.get_external_url_text
+      expect(@news.get_external_url_text).to be_a(String)
+    end
+  end
+
+  it "should return a string for author" do
+    if @news.get_author
+      expect(@news.get_author).to be_a(String)
+    end
+  end
+
+  it "should return a string for title" do
+    expect(@news.get_title).to be_a(String)
+  end
+
+  it "should return an integer for article_media_id" do
+    if @news.get_article_media_id
+      expect(@news.get_article_media_id).to be_a(Integer)
+    end
+  end
+
+  it "should return a string for article_date" do
+    expect(@news.get_article_date).to be_a(String)
+  end
+
+  it "should return a string for thumbnail" do
+    expect(@news.get_thumbnail).to be_a(String)
+  end
+
+  it "should return a string for external_url" do
+    if @news.get_external_url
+      expect(@news.get_external_url).to be_a(String)
+    end
+  end
+
+  it "should return a string for introduction" do
+    if @news.get_introduction
+      expect(@news.get_introduction).to be_a(String)
+    end
+  end
+
+  it "should return an integer for article_fighter_id" do
+    if @news.get_article_fighter_id
+      expect(@news.get_article_fighter_id).to be_a(Integer)
+    end
+  end
+
+  it "should return a string for featured_news_category" do
+    if @news.get_featured_news_category
+      expect(@news.get_featured_news_category).to be_a(String)
+    end
+  end
+
+  it "should return a string for last_modified" do
+    if @news.get_last_modified
+      expect(@news.get_last_modified).to be_a(String)
+    end
+  end
+
+  it "should return a string for url_name" do
+    if @news.get_url_name
+      expect(@news.get_url_name).to be_a(String)
+    end
+  end
+
+  it "should return a string for created" do
+    if @news.get_created
+      expect(@news.get_created).to be_a(String)
+    end
+  end
+
+  it "should return a string for published_start_date" do
+    if @news.get_published_start_date
+      expect(@news.get_published_start_date).to be_a(String)
+    end
+  end
+
 end
