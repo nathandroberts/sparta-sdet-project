58631cbf60e526345592e3387b5f900a699d37d9
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2018-01-07 17:39:42 +0000

    test created for search function

--- features/lib/videos/videos_page.rb
+++ features/lib/videos/videos_page.rb
@@ -8,10 +8,21 @@ class UfcVideosPage
   TITLE_HOLDERS_LINK = 'Title Holders' unless const_defined?(:TITLE_HOLDERS_LINK)
   VIDEOS_LINK = 'Videos' unless const_defined?(:VIDEOS_LINK)
   SUBMIT_NEW_VIDEO_LINK = 'Submit new video' unless const_defined?(:SUBMIT_NEW_VIDEO_LINK)
+  FIGHTER_SEARCH_FIELD_ID = 'fighter_search_field' unless const_defined?(:FIGHTER_SEARCH_FIELD_ID)
+  FIGHTER_SEARCH_BUTTON_ID = 'fighter_search_button' unless const_defined?(:FIGHTER_SEARCH_BUTTON_ID)
+
 
 
   def visit_videos_page
     visit('/videos')
+  end
+
+  def fill_in_fighter_search(name)
+    fill_in(FIGHTER_SEARCH_FIELD_ID, with: name)
+  end
+
+  def click_fighter_search
+    find_button(FIGHTER_SEARCH_BUTTON_ID).click
   end
 
   def video_title_search(text_input)
